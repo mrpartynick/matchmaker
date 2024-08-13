87d19f7e443574f699f67b6b57a5d3296ef2052f
Tsimintiya Nikolay <wwwwat@inbox.ru>
2024-08-13 17:13:13 +0300

    stable build

--- cmd/matchmaker/main.go
+++ cmd/matchmaker/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"log/slog"
+	"matchmaker/config"
+	"matchmaker/internal/matchmaker"
+	"matchmaker/internal/server"
+	"matchmaker/pkg/logging"
+)
 
+func main() {
+	cfg := config.Load("config.yaml")
+
+	logfile := logging.MustLoad()
+	defer logfile.Close()
+
+	mm := matchmaker.NewMatchMaker(cfg)
+
+	s := server.New(cfg, mm)
+	log.Println("Running server")
+	s.Run()
+	slog.Debug("Service stopped")
 }
