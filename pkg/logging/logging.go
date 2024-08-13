0ce88553a996377ea94b579454f71de58f4d4b23
Tsimintiya Nikolay <wwwwat@inbox.ru>
2024-08-13 18:25:57 +0300

    logging bugs fixed

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -24,14 +24,21 @@ func MustLoad() *os.File {
 		if err != nil {
 			log.Fatalf("Failed to create log file: %v", err)
 		}
+		slog.SetLogLoggerLevel(slog.LevelDebug)
 		log.SetOutput(f)
 		return f
 	}
 
-	f, err := os.Open("logs/" + date + ".log")
+	f, err := os.OpenFile("logs/"+date+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("Failed to open log file: %v", err)
 	}
+	/*
+		_, err = f.Write([]byte("Kek"))
+		if err != nil {
+			log.Fatalf("Failed to write to log file: %v", err)
+		}
+	*/
 	log.SetOutput(f)
 	slog.SetLogLoggerLevel(slog.LevelDebug)
 	return f
